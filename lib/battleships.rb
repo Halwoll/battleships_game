e3ccecdc8d6e5ef08bb14638f148c9aa085ce9db
Halwoll <97443146+Halwoll@users.noreply.github.com>
2022-07-28 14:19:48 +0100

    created first instances of battleships

---
+++ lib/battleships.rb
@@ -0,0 +1,13 @@
+class Battleship
+
+  def ship_list
+    ship_list = {
+      "Carrier" => 5,
+      "Battleship" => 4,
+      "Cruiser" => 3,
+      "Submarine" => 3,
+      "Destroyer" => 2
+    }
+  end
+  
+end
---
+++ spec/battleships_spec.rb
@@ -0,0 +1,16 @@
+require 'battleship'
+describe Battleship do
+  context "given a list of ships to choose from" do
+    it "returns a list of ships with their size" do
+      ship = Battleship.new
+      expected_ship_list = {
+        "Carrier" => 5,
+        "Battleship" => 4,
+        "Cruiser" => 3,
+        "Submarine" => 3,
+        "Destroyer" => 2
+      }
+      expect(ship.ship_list).to eq(expected_ship_list)
+    end
+  end
+end
