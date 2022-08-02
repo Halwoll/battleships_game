ea140f7fe11507753566ee6679bc7b2e45eff431
Halwoll <97443146+Halwoll@users.noreply.github.com>
2022-08-02 17:47:35 +0100

    added labels to the width and length

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,19 @@
 class Board
 
+  def initialize(row = 10, column = 10, squares = 1)
+    @row = row
+    @column = column
+    @squares = squares
+  end
+
+  attr_accessor :row, :column, :squares
+
+  def row_label
+    ('1'..'10').to_a
+  end
+  
+  def column_label
+    ('A'..'J').to_a
+  end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,17 @@
 require 'board'
 describe Board do
   context "given a board to play on" do
-    it "returns a board to play on" do
+    it "returns a board with dimensions" do
       board = Board.new
+      expect(board.row).to eql(10)
+      expect(board.column).to eql(10)
+      expect(board.squares).to eql(1)
+    end
+    it "lays out the squares in a grid formation along width and length" do
+      board = Board.new
+      expect(board.row_label).to eql(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
+      expect(board.column_label).to eql(["A", "B" , "C", "D", "E", "F", "G", "H", "I", "J"])
+      # expect(board.squares).to be_a(grid)
     end
   end
 end
