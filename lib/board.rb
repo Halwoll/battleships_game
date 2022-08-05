6dee429605928437d19f4f594a44c3a605ce9272
Halwoll <97443146+Halwoll@users.noreply.github.com>
2022-08-05 14:43:44 +0100

    Updating the board

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,12 @@
 class Board
 
   def initialize(row = 10, column = 10, squares = 1)
-    @row = row
-    @column = column
+    @row = squares * 10
+    @column = squares * 10
     @squares = squares
   end
 
-  attr_accessor :row, :column, :squares
+  attr_accessor :squares, :column, :row
 
   def row_label
     ('1'..'10').to_a
@@ -16,4 +16,8 @@ class Board
     ('A'..'J').to_a
   end
 
+  def grid
+    @row * @column
+  end
+  
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,15 +3,20 @@ describe Board do
   context "given a board to play on" do
     it "returns a board with dimensions" do
       board = Board.new
+      expect(board.squares).to eql(1)
       expect(board.row).to eql(10)
       expect(board.column).to eql(10)
-      expect(board.squares).to eql(1)
     end
     it "labels the width and length" do
       board = Board.new
-      expect(board.row_label).to eql(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
-      expect(board.column_label).to eql(["A", "B" , "C", "D", "E", "F", "G", "H", "I", "J"])
-      # expect(board.squares).to be_a(grid)
+      expected_row_label = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+      expected_column_label = ["A", "B" , "C", "D", "E", "F", "G", "H", "I", "J"]
+      expect(board.row_label).to eql(expected_row_label)
+      expect(board.column_label).to eql(expected_column_label)
+    end
+    it "lays the squares in a grid fashion" do
+      board = Board.new
+      expect(board.grid).to eql(10*10)
     end
   end
 end
