ae5d3684ff9be13c367dcb4ebcf00cd693b12ee4
Halwoll <97443146+Halwoll@users.noreply.github.com>
2022-08-09 16:01:13 +0100

    cleaned up the ship selection

--- lib/ships.rb
+++ lib/ships.rb
@@ -14,44 +14,8 @@ class Ships
     }
   end
 
-  def carrier
-    (@ship << "Carrier").uniq.pop
-  end
-
-  def carrier_size
-    all_ship_size["Carrier"]
-  end
-
-  def battleship
-    (@ship << "Battleship").uniq.pop
-  end
-
-  def battleship_size
-    all_ship_size["Battleship"]
-  end
-
-  def cruiser
-    (@ship << "Cruiser").uniq.pop
-  end
-
-  def cruiser_size
-    all_ship_size["Cruiser"]
-  end
-
-  def submarine
-    (@ship << "Submarine").uniq.pop
-  end
-
-  def submarine_size
-    all_ship_size["Submarine"]
-  end
-
-  def destroyer
-    (@ship << "Destroyer").uniq.pop
-  end
-
-  def destroyer_size
-    all_ship_size["Destroyer"]
+  def select_ship(ship)
+    @ship << ship
   end
   
 end
--- spec/ships_spec.rb
+++ spec/ships_spec.rb
@@ -14,50 +14,9 @@ describe Ships do
     end
   end
   context "gives the ability to select a single ship and see it's size"do
-    it "allowes the player to select the Carrier" do
+    it "allows you to select an individual ship" do
       ship = Ships.new
-      ship.carrier
-      expect(ship.carrier).to eql("Carrier")
-    end
-    it "allowes the player to see how big the carrier is" do
-      ship = Ships.new
-      expect(ship.carrier_size).to eql(5)
-    end
-    it "allowes the player to select the Battleship" do
-      ship = Ships.new
-      ship.battleship
-      expect(ship.battleship).to eql("Battleship")
-    end
-    it "allowes the player to see how big the battleship is" do
-      ship = Ships.new
-      expect(ship.battleship_size).to eql(4)
-    end
-    it "allowes the player to select the Cruiser" do
-      ship = Ships.new
-      ship.cruiser
-      expect(ship.cruiser).to eql("Cruiser")
-    end
-    it "allowes the player to see how big the cruiser is" do
-      ship = Ships.new
-      expect(ship.cruiser_size).to eql(3)
-    end
-    it "allowes the player to select the Submarine" do
-      ship = Ships.new
-      ship.submarine
-      expect(ship.submarine).to eql("Submarine")
-    end
-    it "allowes the player to see how big the submarine is" do
-      ship = Ships.new
-      expect(ship.submarine_size).to eql(3)
-    end
-    it "allowes the player to select the Destroyer" do
-      ship = Ships.new
-      ship.destroyer
-      expect(ship.destroyer).to eql("Destroyer")
-    end
-    it "allowes the player to see how big the destroyer is" do
-      ship = Ships.new
-      expect(ship.destroyer_size).to eql(2)
+      ship.select_ship("Carrier")
     end
   end
 end
